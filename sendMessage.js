92d5e43658222696c6d207a9df2a7802087ee36e
Ascendzor <wow.troymare@gmail.com>
2019-04-28 15:25:05 +1200

    updated region in sendMessage

--- sendMessage.js
+++ sendMessage.js
@@ -1,6 +1,6 @@
 const util = require('util')
 const AWS = require('aws-sdk')
-AWS.config.update({region:'us-east-1'})
+AWS.config.update({region: process.env.region})
 const stage = 'dev'
 
 module.exports = {
