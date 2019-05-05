6378f38d5d82de55a79b680e6f9fb398272f8684
Ascendzor <wow.troymare@gmail.com>
2019-05-05 23:21:33 +1200

    simplified url generation for replying to apigateway

--- sendMessage.js
+++ sendMessage.js
@@ -5,7 +5,7 @@ const stage = process.env.stage
 
 module.exports = {
   toConnection: ({domainName, connectionId, body}) => {
-    const url = util.format(util.format('https://%s/%s', domainName, stage)) //construct the needed url
+    const url = `https://${domainName}/${stage}`
     const apigatewaymanagementapi = new AWS.ApiGatewayManagementApi({apiVersion: '2018-11-29', endpoint: url})
     return apigatewaymanagementapi.postToConnection({
       ConnectionId: connectionId,
